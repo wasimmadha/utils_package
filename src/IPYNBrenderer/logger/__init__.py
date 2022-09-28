17d110b7ba647878ff329ba879472c29968f4eaf
wasimmadha <wasimmadh2004@gmail.com>
2022-09-29 03:53:06 +0530

    code formatting

--- src/IPYNBrenderer/logger/__init__.py
+++ src/IPYNBrenderer/logger/__init__.py
@@ -1,25 +1,25 @@
-import logging 
+import logging
 from datetime import datetime
-import os, sys
+import os
+import sys
 
-LOG_DIR="./logs"
+LOG_DIR = "./logs"
 
-CURRENT_TIME_STAMP= f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
-LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
+LOG_FILE_NAME = f"log_{CURRENT_TIME_STAMP}.log"
 
-os.makedirs(LOG_DIR,exist_ok=True)
+os.makedirs(LOG_DIR, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(LOG_DIR ,LOG_FILE_NAME)
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
 
-logging.basicConfig(filename=LOG_FILE_PATH,
-    filemode="w",
-    format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s'
-    ,level=logging.INFO,
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
     handlers=[
+        logging.FileHandler(LOG_FILE_PATH),
         logging.StreamHandler(sys.stdout),
-        logging.FileHandler(LOG_FILE_PATH)
-    ]
+    ],
 )
 
 logger = logging.getLogger("IPYNBrenderer")
