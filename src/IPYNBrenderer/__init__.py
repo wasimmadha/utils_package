17288cee2fffd9f72eec47022ccb3e01d0232cd2
wasimmadha <wasimmadh2004@gmail.com>
2022-09-29 04:35:46 +0530

    added youtube.py

--- src/IPYNBrenderer/__init__.py
+++ src/IPYNBrenderer/__init__.py
@@ -1,2 +1,2 @@
-from IPYNBrenderer.youtube import get_time_info, render_YouTube_video
+from IPYNBrenderer.youtubeRender import get_time_info, render_YouTube_video
 from IPYNBrenderer.site import render_site, is_valid
